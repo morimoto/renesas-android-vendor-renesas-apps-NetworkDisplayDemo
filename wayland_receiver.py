4918d1562ff8561c94fecaa0afc03d2223dab63e
Oleksandr Shaposhnikov <o.shaposhnikov@globallogic.com>
2018-04-23 15:37:38 +0300

    Ported AGL demo server to Python 3, updated instructions.
    AGL doesn't have required Python 2 modules like select, which is
    needed for the script to run. Python 3 does have it, thus this script
    has been ported to Python 3.
    
    Reflected recent changes in README and corrected mistakes.
    
    SocketServer.py is taken from Python 3.5 release.
    
    Change-Id: I9645827af705ee9a53cd49e12bb81e399c262fee
    Signed-off-by: Oleksandr Shaposhnikov <o.shaposhnikov@globallogic.com>
    (cherry picked from commit 5014255ff6c408ed2865de838e88a658d2aadf80)

--- wayland_receiver.py
+++ wayland_receiver.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from threading import Thread
 from subprocess import Popen, PIPE, STDOUT
@@ -11,36 +11,62 @@ PORT = 53515
 IP = '172.22.89.45'
 
 bufferSize = 1024
-meta_data = '{"port":%d,"name":"TestReceiver @ %s","id":"%s","width":1280,"height":960,"mirror":"h264","audio":"pcm","subtitles":"text/vtt","proxyHeaders":true,"hls":false,"upsell":true}' % (PORT, REC_NAME, IP)
+meta_data = '{\
+"port":%d,\
+"name":"TestReceiver @ %s",\
+"id":"%s",\
+"width":1280,"height":960,\
+"mirror":"h264",\
+"audio":"pcm",\
+"subtitles":"text/vtt",\
+"proxyHeaders":true,\
+"hls":false,\
+"upsell":true\
+}' % (PORT, REC_NAME, IP)
 
 SAVE_TO_FILE = False
 
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
-        p = Popen(['gst-launch-1.0','-vvv', 'fdsrc', "do-timestamp=true", '!', 'h264parse','config-interval=1', 'disable-passthrough=true', '!', 'omxh264dec', 'no-reorder=true', '!', 'waylandsink'], stdin=PIPE, stdout=PIPE)
+        p = Popen([
+            'gst-launch-1.0',
+            '-vvv',
+            'fdsrc',
+            'do-timestamp=true',
+            '!',
+            'h264parse',
+            'config-interval=1',
+            'disable-passthrough=true',
+            '!',
+            'omxh264dec',
+            'no-reorder=true',
+            '!',
+            'waylandsink',
+        ], stdin=PIPE, stdout=PIPE)
         skiped_metadata = False
         while True:
             data = self.request.recv(bufferSize)
             if data == None or len(data) <= 0:
                 break
             if not skiped_metadata:
-                print "Client connected, addr: ", self.client_address[0]
-                if data.find('\r\n\r\n') > 0:
-                    last_ctrl = data.find('\r\n\r\n') + 4
-                    print 'Recv control data: ', data[0:last_ctrl]
+                print('Client connected, addr: ', self.client_address[0])
+                found_pos = data.find(b'\r\n\r\n')
+                if found_pos > 0:
+                    last_ctrl = found_pos + 4
+                    print('Recv control data: ', data[0:last_ctrl])
                     if len(data) > last_ctrl:
                         p.stdin.write(data[last_ctrl:])
                 skiped_metadata = True
             else:
-                #print "write data[", len(data), "]"
+                #print("Write data (%d b)" % len(data))
                 p.stdin.write(data)
         p.kill()
 
 
-def resp_hello(ip, port):
+def resp_hello(addr):
     send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    send_sock.sendto(meta_data, (ip, port))
+    send_sock.sendto(meta_data.encode(), addr)
 
 
 def handle_discovery():
@@ -52,10 +78,11 @@ def handle_discovery():
         if len(result[0]) <= 0:
             continue
         msg, address = result[0][0].recvfrom(bufferSize)
-        print 'Receive broadcast msg: ', msg
-        if msg == 'hello':
-            print 'Got discover msg, src ip: %s, port: %d' % (address[0], address[1])
-            resp_hello(address[0], address[1])
+        print( 'Receive broadcast msg: ', msg)
+        print(address)
+        if msg == b'hello':
+            print( 'Got discover msg, src ip: %s, port: %d' % address)
+            resp_hello(address)
 
 
 if __name__ == "__main__":
